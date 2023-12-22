8cd54cb94742485ff761e5ca1156eda51eb0de16
lukejohn <lukejohnbiblestudy@yahoo.com>
2023-12-22 09:06:37 -0500

    First version

---
+++ Emilymap.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+
+public class Emilymap {
+     public static void main(String[] args) {
+
+    HashMap<String, Double> employees = new HashMap<String, Double>();
+
+    employees.put("john", 123.3);
+    employees.put("luke", 234.4);
+
+    System.out.println(employees);
+
+    
+    double result = 0; 
+
+    for (Double v : employees.values()) {  // for each value
+        result +=  v.doubleValue();
+    }
+
+    System.out.println("The sume is " + result);
+  }
+ 
+}
